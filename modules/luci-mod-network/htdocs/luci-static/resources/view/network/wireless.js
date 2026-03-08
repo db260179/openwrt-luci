1759ff68701ea37760a9e5948e0cf3a1af4f16a3
David Bentham <dbentham@fgs-uk.com>
2026-03-08 12:22:10 +0000

    luci-mod-network:wireless - Set max_inactivity placeholder value to 1200 seconds as this is a better value than 5 minutes for clients to disconnect
    Increase the recommended gtk rekeying to 14400 based on my findings

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1242,7 +1242,7 @@ return view.extend({
 
 					o = ss.taboption('advanced', form.Value, 'wpa_group_rekey', _('Time interval for rekeying GTK'), _('sec'));
 					o.optional    = true;
-					o.placeholder = 600;
+					o.placeholder = 14400;
 					o.datatype    = 'uinteger';
 
 					o = ss.taboption('advanced', form.Flag , 'skip_inactivity_poll', _('Disable Inactivity Polling'));
@@ -1256,7 +1256,7 @@ return view.extend({
 
 					o = ss.taboption('advanced', form.Value, 'max_inactivity', _('Station inactivity limit'), _('802.11v: BSS Max Idle. Units: seconds.'));
 					o.optional    = true;
-					o.placeholder = 300;
+					o.placeholder = 1200;
 					o.datatype    = 'uinteger';
 
 					o = ss.taboption('advanced', form.Value, 'max_listen_interval', _('Maximum allowed Listen Interval'));
